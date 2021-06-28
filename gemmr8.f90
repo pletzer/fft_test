a1afb0ecc64e88a44ba46f8e96b43239e9883ee4
Alex Pletzer <alexander@gokliya.net>
2021-06-28 05:09:17 +0000

    + gemm tests

---
+++ gemmr8.f90
@@ -0,0 +1,29 @@
+program test
+    ! gfortran -O3 matr4.f90 -o matr4
+    implicit none
+    integer :: n, stat, i, j
+    character(len=32) :: nstr
+    real(8), allocatable :: fs(:), sines(:, :), res(:)
+    real :: dt, tic, toc
+    real(8), parameter :: pi = 4*atan(1.), alpha = 1, beta = 0
+
+    if (command_argument_count() < 1) then
+        stop 'ERROR must supply array size as command line argument'
+    endif
+    call get_command_argument(1, nstr)
+    read(nstr, *, iostat=stat) n
+
+    allocate(fs(n), sines(n, n), res(n))
+    do j = 1, n
+        fs(j) = j
+        do i = 1, n
+            sines(i, j) = sin(i*j*pi/real(n + 1, 8))
+        enddo
+    enddo
+
+    call cpu_time(tic)
+    call dgemm('N', 'N', n, 1, n, alpha, sines, n, fs, n, beta, res, n)
+    call cpu_time(toc)
+    dt = toc - tic
+    print *,'gemmr8 n = ', n, ' time = ', dt, ' secs res = ', sum(res)
+end program test
