d6f8c8151bcd9ebe1b6377d39baef8abe97f4939
Alex Pletzer <alexander@gokliya.net>
2021-06-30 14:56:30 +1200

    new results

---
+++ plot_resultsqw.py
@@ -0,0 +1,25 @@
+import pandas
+from matplotlib import pylab
+
+df = pandas.read_csv('resultsqw.csv')
+print(df)
+
+styles = ['r-', 'r--', 'g-', 'g--']
+i = 0
+legs = []
+for colname in df.columns:
+	if colname == 'n':
+		continue
+	print(colname)
+	print(df['n'])
+	print(df[colname])
+	print(i)
+	print(styles[i])
+	legs.append(colname)
+	pylab.loglog(df['n'], df[colname], styles[i])
+	i += 1
+pylab.title('sine/cosine expansion of QX.W1')
+pylab.legend(legs)
+pylab.xlabel('n')
+pylab.ylabel('time [s]')
+pylab.show()
