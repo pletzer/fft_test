385d1b08d806f30dfd13cf9497a60ec4140eb6a0
Alex Pletzer <alexander@gokliya.net>
2021-06-30 21:37:32 +0000

    fixed casting bug

--- fftqwr8.f90
+++ fftqwr8.f90
@@ -14,11 +14,11 @@ program test
     implicit none
     integer :: n, stat, i, j
     character(len=32) :: nstr
-    real, allocatable :: w(:), res(:)
-    complex, allocatable :: wbar(:)
+    real(8), allocatable :: w(:), res(:)
+    complex*16, allocatable :: wbar(:)
     type(dfti_descriptor), pointer :: handle
-    real, parameter :: pi = 4*atan(1.)
-    real :: a, b
+    real(8), parameter :: pi = 4._8*atan(1._8)
+    real(8) :: a, b
     real :: dt, tic, toc
 
     if (command_argument_count() < 1) then
@@ -27,7 +27,7 @@ program test
     call get_command_argument(1, nstr)
     read(nstr, *, iostat=stat) n
 
-    stat = DftiCreateDescriptor(handle, DFTI_SINGLE, DFTI_COMPLEX, 1, n)
+    stat = DftiCreateDescriptor(handle, DFTI_DOUBLE, DFTI_COMPLEX, 1, n)
     call print_error(stat, 'DftiCreateDescriptor')
 
     stat = DftiCommitDescriptor(handle)
@@ -39,13 +39,13 @@ program test
         w(i) = i
     enddo
 
-    a = sqrt(1./n)
-    b = sqrt(2./n)
+    a = sqrt(1._8/n)
+    b = sqrt(2._8/n)
     call cpu_time(tic)
     wbar(1    ) = a * w(1)
     wbar(1+n/2) = a * w(1 + n/2)
     do j = 2, n/2
-        wbar(j    ) = b*( w(j) + cmplx(0., 1.)*w(j+n/2) )
+        wbar(j    ) = b*( w(j) + cmplx(0._8, 1._8)*w(j+n/2) )
         wbar(j+n/2) = 0
     enddo
     stat = dftiComputeForward(handle, wbar)
@@ -54,13 +54,13 @@ program test
     call cpu_time(toc)
     dt = toc - tic
 
-    print *,'fftqwr4 n = ', n, ' time = ', dt, ' secs res = ', sum(res)
+    print *,'fftqwr8 n = ', n, ' time = ', dt, ' secs res = ', sum(res)
 
     stat = dftiFreeDescriptor(handle)
     call print_error(stat, 'dftiFreeDescriptor')
 
     ! print *,'res '
-    ! do i = 1, n
+    ! do i = 1, 100 
     !    print *,res(i)
     ! enddo
 
--- matqwr8.f90
+++ matqwr8.f90
@@ -3,10 +3,10 @@ program test
     implicit none
     integer :: n, stat, i, j
     character(len=32) :: nstr
-    real, allocatable :: q(:,:), w(:), res(:)
+    real(8), allocatable :: q(:,:), w(:), res(:)
     real :: dt, tic, toc
-    real, parameter :: pi = 4*atan(1.)
-    real :: a, b
+    real(8), parameter :: pi = 4._8*atan(1._8)
+    real(8) :: a, b
 
     if (command_argument_count() < 1) then
         stop 'ERROR must supply array size as command line argument'
@@ -17,17 +17,17 @@ program test
     allocate(q(n, n), w(n), res(n))
 
     do i = 1, n
-        w(i) = real(i)
+        w(i) = real(i, 8)
     enddo
 
-    a = sqrt(1./n)
-    b = sqrt(2./n)
+    a = sqrt(1._8/n)
+    b = sqrt(2._8/n)
     do i = 1, n
         q(i, 1    ) = a
-        q(i, 1+n/2) = a * (-1)**(i-1)
+        q(i, 1+n/2) = a * (-1._8)**(i-1)
         do j = 2, n/2
-            q(i, j    ) = b * cos((i-1)*(j-1)*2*pi/real(n, 4))
-            q(i, j+n/2) = b * sin((i-1)*(j-1)*2*pi/real(n, 4))
+            q(i, j    ) = b * cos((i-1)*(j-1)*2*pi/real(n, 8))
+            q(i, j+n/2) = b * sin((i-1)*(j-1)*2*pi/real(n, 8))
         enddo
     enddo
 
@@ -35,10 +35,10 @@ program test
     res = matmul(q, w)
     call cpu_time(toc)
     dt = toc - tic
-    print *,'matqwr4 n = ', n, ' time = ', dt, ' secs res = ', sum(res)
+    print *,'matqwr8 n = ', n, ' time = ', dt, ' secs res = ', sum(res)
 
     ! print *,'res '
-    ! do i = 1, n
+    ! do i = 1, 100
     !    print *,res(i)
     ! enddo
 
